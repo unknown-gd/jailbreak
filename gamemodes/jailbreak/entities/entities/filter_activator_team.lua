043c0c7bcbf20e1debf58e5c9073e2257d3ec1f7
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2023-11-12 06:27:18 +0400

    Lua removed

--- gamemodes/jailbreak/entities/entities/filter_activator_team.lua
+++
@@ -1,5 +0,0 @@
-ENT.Base = "base_filter"
-
-function ENT:PassesFilter( entity, ply )
-    return IsValid( ply ) and ply:IsPlayer() and ply:Alive() and ply:Team() == ( entity:GetInternalVariable( "TeamNum" ) + 1 )
-end
